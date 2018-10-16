35e56733604af820d83693398b908dd00fb84914
Tomat7 <star0413@gmail.com>
2018-10-16 13:12:52 +0300

    cosmetic changes

--- examples/ADCmulti_three/ADCmulti_three.ino
+++ examples/ADCmulti_three/ADCmulti_three.ino
@@ -5,24 +5,27 @@
 #include "ADCmulti.h"
 ADCmulti AnalogMulti(A1, A2, A3);
 
+long int msMillis;
+
 void setup()
 {
   Serial.begin(115200);
-  delay(500);
-
+  delay(300);
   AnalogMulti.init();
-
-  delay(500);
+  msMillis = millis();
+  delay(200);
 }
 
 void loop()
 {
   AnalogMulti.check();
-
-  Serial.println(AnalogMulti.read(A1));
-  Serial.println(AnalogMulti.read(A2));
-  Serial.println(AnalogMulti.read(A3));
-  Serial.println("+++");
-
-  delay(1000);
+  
+  if ((millis() - msMillis) > 1000)
+  {
+    Serial.println(AnalogMulti.read(A1));
+    Serial.println(AnalogMulti.read(A2));
+    Serial.println(AnalogMulti.read(A3));
+    Serial.println("+++");
+    msMillis = millis();
+  }
 }
